271689636389b8efb5368b9e086c3be5b9362dac
Ronny Coste <ronny21.97@gmail.com>
2022-03-29 21:57:28 -0400

    Trying to add a carousel

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,12 +9,6 @@ const About = () => (
       voluptatibus distinctio recusandae autem esse explicabo molestias officia
       placeat, accusamus aut saepe.
     </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
   </Main>
 );
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -242,6 +242,116 @@ const Index = () => {
           </div>
         </section>
       </div>
+
+      {/* Aaaannnnnddd we know the drill */}
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide relative"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators absolute inset-x-0 bottom-0 mb-4 flex justify-center p-0">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner relative w-full overflow-hidden">
+          <div className="carousel-item active relative float-left w-full">
+            <div
+              className="relative overflow-hidden bg-cover bg-no-repeat"
+              // style="background-position: 50%;"
+            >
+              <img
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(123).jpg"
+                className="block w-full"
+              />
+              <div className="absolute inset-0 h-full w-full overflow-hidden bg-black bg-fixed opacity-50"></div>
+            </div>
+            <div className="carousel-caption absolute hidden text-center md:block">
+              <h5 className="text-xl">First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item relative float-left w-full">
+            <div
+              className="relative overflow-hidden bg-cover bg-no-repeat"
+              // style="background-position: 50%;"
+            >
+              <img
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(124).jpg"
+                className="block w-full"
+              />
+              <div className="absolute inset-0 h-full w-full overflow-hidden bg-black bg-fixed opacity-50"></div>
+            </div>
+            <div className="carousel-caption absolute hidden text-center md:block">
+              <h5 className="text-xl">Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item relative float-left w-full">
+            <div
+              className="relative overflow-hidden bg-cover bg-no-repeat"
+              // style="background-position: 50%;"
+            >
+              <img
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(125).jpg"
+                className="block w-full"
+              />
+              <div className="absolute inset-0 h-full w-full overflow-hidden bg-black bg-fixed opacity-50"></div>
+            </div>
+            <div className="carousel-caption absolute hidden text-center md:block">
+              <h5 className="text-xl">Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev absolute inset-y-0 left-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon inline-block bg-no-repeat"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next absolute inset-y-0 right-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon inline-block bg-no-repeat"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </Main>
   );
 };
