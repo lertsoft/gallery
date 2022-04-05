0322f07d3225f5853fcf3fc12965d4f5e210c86f
Ronny Coste <ronny21.97@gmail.com>
2022-04-05 11:01:51 -0400

    Revert "changes for template"
    This reverts commit 0131c908184fae1c50b9c4476dbf396f90ce12c2.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,6 +8,37 @@ const About = () => (
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
       voluptatibus distinctio recusandae autem esse explicabo molestias officia
       placeat, accusamus aut saepe.
+      <div id="carouselExampleCaptions" className="" data-bs-ride="carousel">
+        <div className="absolute inset-0 h-full w-full overflow-hidden bg-black bg-fixed opacity-50">
+          <iframe
+            className=""
+            src="https://www.youtube.com/embed/X52cKIhJTdI"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+
+          <div className="">
+            <h3 className=""> Camera assistant (AC) </h3>
+          </div>
+        </div>
+        <div className="absolute inset-0 h-full  overflow-hidden bg-black bg-fixed opacity-0">
+          <div className="">
+            <iframe
+              className=""
+              src="https://www.youtube.com/embed/sA_Uk9Tpp8c"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="">
+              <h3 className=""> Camera assistant (AC)</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </p>
   </Main>
 );
---
+++ src/pages/nextJsImage.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Image from 'next/image';
+import { PhotoProps } from 'react-photo-album';
+
+type NextJsImageProps = PhotoProps & {
+  wrapperProps?: React.HTMLAttributes<HTMLDivElement>;
+};
+
+const nextJsImage = ({ photo, imageProps, wrapperProps }: NextJsImageProps) => {
+  const { width, height } = photo;
+  const { src, alt, title, style, sizes, className, id, onClick } = imageProps;
+  const { style: wrapperStyle, ...restWrapperProps } = wrapperProps ?? {};
+
+  return (
+    <div
+      style={{
+        width: style.width,
+        padding: style.padding,
+        marginBottom: style.marginBottom,
+        ...wrapperStyle,
+      }}
+      {...restWrapperProps}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        title={title}
+        sizes={sizes}
+        width={width}
+        height={height}
+        className={className}
+        onClick={onClick}
+        id={id}
+      />
+    </div>
+  );
+};
+
+export default nextJsImage;
