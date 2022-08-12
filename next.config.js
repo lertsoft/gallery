1826c98bdfb0bebc6a9846795674e77aced92b61
Ronny Coste <ronny21.97@gmail.com>
2022-08-12 13:53:10 -0400

    Forgot to add the service worker

--- next.config.js
+++ next.config.js
@@ -15,3 +15,15 @@ module.exports = withBundleAnalyzer({
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
 });
+
+// Generate the service worker
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  // ...before
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+});
