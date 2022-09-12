a9e2080faee0097d80a827cdad62ff0cd8539e4c
Ronny Coste <ronny21.97@gmail.com>
2022-09-12 18:21:03 -0400

    added offline functionality

---
+++ src/pages/_offline.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Fallback = () => (
+  <>
+    <Head>
+      <title>Curated By</title>
+    </Head>
+    <h1>This is offline fallback page</h1>
+    <h2>You are disconnected from the internet in this moment.</h2>
+  </>
+);
+
+export default Fallback;
